b871b0afa8160bdfbb83b98a3f9eab451200c2c9
DreamFlower14 <sunghh1004@naver.com>
2022-03-13 14:23:21 +0900

    생활코딩

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -1,7 +1,9 @@
 public class Home {
     public static void main(String []args){
-        int a = 1;
-        System.out.println(" + 1 = 2 ");
+       /* int a = 1, b= 2, c = 3;
+        System.out.printf(" %d + %d = %d ", a, b, c); */
+        System.out.println(Math.PI);
+        System.out.println(Math.floor(3.9));
 
     }
 
