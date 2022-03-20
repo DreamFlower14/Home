82ad5533e56394d8ad99b6b8ad90a5b65cda51db
DreamFlower14 <sunghh1004@naver.com>
2022-03-20 14:01:12 +0900

    나이 확인

--- src/main/java/Double.java
+++ src/main/java/Double.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;     // Scanner를 사용하기 위해서 임포트(자바에 dlc를 꼽는거)
 public class Double {
     public static void main(String[] args){
-
-        // 목표 : 이름과 나이를 입력했을때 이름은 같고 나이는 19세 이상이면 구구단을 사용할 수 있게하기. (Scanner사용하기)(if문 사용하기)(for문 사용하기)
-        Scanner in = new Scanner(System.in);      // 입력하는 변수 " in " 를 사용하겠다 선언, 후 Scanner를 데이터에 저장하라는 new를 사용   (System.in)은 데이터에 저장하는거
+        // 목표 : 이름과 나이를 입력()했을때 이름은 같고 나이는 19세 이상이면 구구단을 사용할 수 있게하기. (Scanner사용하기)(if문 사용하기)(for문 사용하기)
+        Scanner in = new Scanner(System.in); // 입력하는 변수 " in " 를 사용하겠다 선언, 후 Scanner를 데이터에 저장하라는 new를 사용   (System.in)은 데이터에 저장하는거
         System.out.print("이름을 입력해주세요 : ");    // 이름을 입력해주세요 출력
         String i = in.nextLine();//  이름 저장받는 변수인  " i " 생성
         String k = "김성현";   // 이 이름과 같은 이름을 입력해야지 구구단이 작동
@@ -11,24 +10,18 @@ public class Double {
         if(k.equals(i)){    // (기준).eqauls(넣는값) : 기준과 넣는 값이 같을 때 true
             System.out.print("나이를 입력해주세요 : ");  // 나이를 입력해주세요 출력
             int j = in.nextInt();  // ※ carmel 표기법으로 중간에 대문자 넣어주기
-            if(j>=19){ System.out.println("성인입니다. 구구단 프로그램 사용허가완료\n"); // 19세 이상이면 출력
+            if(j>=20)
+            { System.out.println("성인입니다. 구구단 프로그램 사용허가완료\n"); // 19세 이상이면 출력
                 System.out.print("구구단 몇단 : ");
-                int l = in.nextInt(); // 구구단 몇 단 뒤에 입력한 숫자를 기준으로 구구단 생성
-                int h = 0;  //
+                 int l = in.nextInt(); // 구구단 몇 단 뒤에 입력한 숫자를 기준으로 구구단 생성
                 System.out.println( l + "단" );  // (입력한 값)단
-                System.out.println( l + " x " + ++h + " = " + (l*h));
-                System.out.println( l + " x " + ++h + " = " + (l*h));
-                System.out.println( l + " x " + ++h + " = " + (l*h));
-                System.out.println( l + " x " + ++h + " = " + (l*h));
-                System.out.println( l + " x " + ++h + " = " + (l*h));
-                System.out.println( l + " x " + ++h + " = " + (l*h));
-                System.out.println( l + " x " + ++h + " = " + (l*h));
-                System.out.println( l + " x " + ++h + " = " + (l*h));
-                System.out.println( l + " x " + ++h + " = " + (l*h) + "\n");
+                int h;
+                for( h = 1; h <= 8; h++ )
+                  { System.out.println( l + " x " + h + " = " + (l*h)); }
+                System.out.println( l + " x " + h + " = " + (l*h) + "\n"); // 증감자를 안써주는 이유는 for문의 조건 끝dp h++에서 1 증가돼서 나오기 때문
                 System.out.println("구구단 프로그램 종료");
             }
-            else{ System.out.println("미성년자 로그인 실패"); } // 19세 미만일 때 출력
+              else{ System.out.println("미성년자 로그인 실패"); } //
                         }
-            else{ System.out.println("이름 불일치 로그인 실패"); }  // 기준 값이랑 입력한 값이 다를때 출력
-
+             else{ System.out.println("이름 불일치 로그인 실패"); }  //
     }}
