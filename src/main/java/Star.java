fcaed0506b0acf6e6988380f0f2b68779786b885
DreamFlower14 <sunghh1004@naver.com>
2022-03-21 21:54:56 +0900

    반복문

--- src/main/java/Star.java
+++ src/main/java/Star.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Star {
     public static void main(String[] args){
-
+        Scanner scan = new Scanner(System.in);
+        System.out.print("구구단 몇 단 : ");
+        int F = scan.nextInt();
+        //System.out.println(F);
+        int g;
+        for( g = 1; g < 10; g++)
+        { System.out.printf("%d x %d = %d\n", F, g, (F*g));}
+        // 반복문  printf로 구구단 만들기
     }
 }
