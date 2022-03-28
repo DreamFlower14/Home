617137956db1f05c5728bcf11ea3aca8163d7706
DreamFlower14 <sunghh1004@naver.com>
2022-03-28 21:46:49 +0900

    2022-03-28 출력문 사용

---
+++ src/main/java/Homework.java
@@ -0,0 +1,13 @@
+public class Homework {
+    public static void main(String[] args) {
+        System.out.print("큰 따음표 사이에 입력한 그대로 출력해주는 프린트");
+        System.out.println();
+        System.out.println(" 출력하고 한 줄을 넘겨주는 프린트ln");
+        String i = "프린트f";
+        System.out.printf("서식을 지정해주는 %s", i);
+        System.out.println();
+        System.out.printf("프린트 f에서는 큰 따음표 안의 쓰는 것은 모두 문자로 인식한다.");
+        System.out.println();
+        System.out.printf("그리고 \n을 하면 그 자리에서 줄을 넘긴다");
+    }
+}
