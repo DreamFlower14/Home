652e430fa2cca79b170ac159689519522be5555a
DreamFlower14 <sunghh1004@naver.com>
2022-03-29 18:18:42 +0900

    2022-03-29 연산자

--- src/main/java/Homework.java
+++ src/main/java/Homework.java
@@ -1,13 +1,20 @@
 public class Homework {
     public static void main(String[] args) {
-        System.out.print("큰 따음표 사이에 입력한 그대로 출력해주는 프린트");
-        System.out.println();
-        System.out.println(" 출력하고 한 줄을 넘겨주는 프린트ln");
-        String i = "프린트f";
-        System.out.printf("서식을 지정해주는 %s", i);
-        System.out.println();
-        System.out.printf("프린트 f에서는 큰 따음표 안의 쓰는 것은 모두 문자로 인식한다.");
-        System.out.println();
-        System.out.printf("그리고 \n을 하면 그 자리에서 줄을 넘긴다");
+        int a,b = 5, c = 3;
+
+        a = b + c;
+        System.out.printf(" %d + %d = %d\n", b, c, a); // 더하기
+
+        a = b - c;
+        System.out.printf(" %d - %d = %d\n", b, c, a); // 빼기
+
+        a = b * c;
+        System.out.printf(" %d * %d = %d\n", b, c, a); // 곱하기
+
+        a = b / c;
+        System.out.printf(" %d / %d = %d\n", b, c, a); // 나누기
+
+        a = b % c;
+        System.out.printf(" %d %% %d = %d\n", b, c, a); // 나머지 값을 대입
     }
 }
