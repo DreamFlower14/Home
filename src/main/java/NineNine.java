931ec96ab559f333a0b546fa815bd7206bc0dc6d
DreamFlower14 <sunghh1004@naver.com>
2022-04-02 12:14:10 +0900

    2022-04-02

--- src/main/java/NineNine.java
+++ src/main/java/NineNine.java
@@ -1,11 +1,27 @@
 public class NineNine {
     public static void main(String[] args) {
-        for(int j = 1; j <= 9; j++){
-            System.out.println( j + " 단 " );
-            for(int i = 1; i <= 9; i++){
-                System.out.println(j + " x " + i + " = " + (j*i));
-            }
-            System.out.println();
-        }
+        int a = 10;
+
+        a++;
+        System.out.printf(" a ++ ==> %d \n", a);  // 계산을 나중에 하기 때문에 후치 증가 연산자라고 부른다
+
+        a--;
+        System.out.printf(" a -- ==> %d \n", a);  // 계산을 먼저하기 때문에 선치 증가 연산자라고 부른다
+
+        a +=5;
+        System.out.printf(" a += 5 ==> %d \n", a);
+
+        a -= 5;
+        System.out.printf(" a -= 5 ==> %d \n", a);
+
+        a *= 5;
+        System.out.printf(" a *= 5 ==> %d \n", a);
+
+        a /= 5;
+        System.out.printf(" a /= 5 ==> %d \n", a);
+
+        a %= 5;
+        System.out.printf(" a %%= 5 ==> %d \n", a); // %는 출력할 땐 한개만 쓰면 안되기 때문에 %%로 써서 표현한다
+
     }
 }
