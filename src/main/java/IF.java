baa5fe7480d0587c555ba0eb55539aed4a066269
DreamFlower14 <sunghh1004@naver.com>
2022-04-04 21:50:16 +0900

    2022-04-04 else if

--- src/main/java/IF.java
+++ src/main/java/IF.java
@@ -3,14 +3,20 @@ public class IF {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        System.out.print("정수를 입력해줘요 : ");
+        System.out.print("점수를 입력해줘요 : ");
         int a = s.nextInt();
 
 
-        if (a < 100){
-            System.out.println("100보다 작군요");
+        if (a >= 90){
+            System.out.println("A");
+        }else if ( a >= 80 ){
+            System.out.println("B");
+        }else if ( a >= 70 ){
+            System.out.println("c");
+        }else if ( a >= 60){
+            System.out.println("D");
         }else{
-            System.out.println("100과 같거나 크군요");
+            System.out.println("f");
         }
     }
 }
