7f726b0edc65953ebd14a9acc5181e5771bdce31
DreamFlower14 <sunghh1004@naver.com>
2022-06-01 13:27:15 +0900

    22-06-01

---
+++ src/main/java/CodeUp/C1011.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1011 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        char a = s.next().charAt(0);
+        System.out.println(a);
+    }
+}
