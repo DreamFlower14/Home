72cb753a8cc69b2de4712b8ad5f1a03e58b249ab
DreamFlower14 <sunghh1004@naver.com>
2022-06-04 22:45:07 +0900

    22-06-04

---
+++ src/main/java/CodeUp/C1012.java
@@ -0,0 +1,11 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1012 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        Float a = s.nextFloat();
+        System.out.printf("%f", a);
+    }
+}
