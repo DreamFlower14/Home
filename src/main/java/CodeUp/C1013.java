c24f2cd9f64f4d0d825581cfd5e557507c3744ea
DreamFlower14 <sunghh1004@naver.com>
2022-06-09 23:25:16 +0900

    22-06-09

---
+++ src/main/java/CodeUp/C1013.java
@@ -0,0 +1,15 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class C1013 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        char a,b;
+        a = s.next().charAt(0);
+        b = s.next().charAt(0);
+
+        System.out.print(b+" ");
+        System.out.println(a);
+    }
+}
