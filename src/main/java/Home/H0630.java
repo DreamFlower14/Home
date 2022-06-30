f8965d9a402f3a02b9b2cdd4bec6e542583637c3
DreamFlower14 <sunghh1004@naver.com>
2022-06-30 13:53:47 +0900

    22-06-30

---
+++ src/main/java/Home/H0630.java
@@ -0,0 +1,11 @@
+package Home;
+
+public class H0630 {
+    public static void main(String[] args) {
+        // 문자열을 붙여주는 함수
+        StringBuilder sb = new StringBuilder("hello ");
+        sb.append("world").append("1").append("2");
+
+        System.out.println(sb);
+    }
+}
