191db265eb84a44fa259ae9cde348ba11ca58634
DreamFlower14 <sunghh1004@naver.com>
2022-07-20 20:47:55 +0900

    22-07.20

---
+++ src/main/java/baekjoon/B11654.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class B11654 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        char a = s.next().charAt(0);
+
+        System.out.println((int)a);
+    }
+}
